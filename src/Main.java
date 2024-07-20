390b7b2c7661851d019fb2de286ccb98e030ce34
rranl <rranlevi@gmail.com>
2024-07-20 15:15:05 +0300

    Client

--- src/Main.java
+++ src/Main.java
@@ -6,33 +6,57 @@ import com.google.gson.reflect.TypeToken;
 
 import java.lang.reflect.Type;
 import java.util.List;
+import javax.swing.*;
+import java.awt.FlowLayout;
 
 public class Main {
     public static void main(String[] args) {
-        RestWrapper restWrapper = new RestWrapper();
+        // Create a new frame
+        JFrame frame = new JFrame("Swing Button Example");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(300, 200);
 
-        // Test POST request
-        RequestStatus postStatus = restWrapper.sendPost(RestPath.POST_PATH, "Test Payload");
-        System.out.println("POST Request Success: " + postStatus.isSuccess());
-        System.out.println("POST Request Message: " + postStatus.getMessage());
+        // Create a panel to hold the button
+        JPanel panel = new JPanel();
+        panel.setLayout(new FlowLayout());
 
-        // Test GET request
-        RequestStatus getStatus = restWrapper.sendGet(RestPath.GET_PATH);
-        System.out.println("GET Request Success: " + getStatus.isSuccess());
-        System.out.println("GET Request Message: " + getStatus.getMessage());
+        // Create a new button
+        JButton button = new JButton("Click Me!");
+        JLabel lbl1 = new JLabel("Harel the gay");
+        // Add the button to the panel
+        panel.add(button);
+        panel.add(lbl1);
+        // Add the panel to the frame
+        frame.add(panel);
 
-        // Test GET request for devices
-        RequestStatus getDiscDevicesStatus = restWrapper.sendGet(RestPath.GET_DISC_DEVICES_PATH);
-        if (getStatus.isSuccess()) {
-            Gson gson = new Gson();
-            Type listType = new TypeToken<List<HouseholdDevice>>() {}.getType();
-            List<HouseholdDevice> devices = gson.fromJson(getDiscDevicesStatus.getMessage(), listType);
+        // Make the frame visible
+        frame.setVisible(true);
 
-            for (HouseholdDevice device : devices) {
-                System.out.println("Device: " + device.getDeviceName() + ", Room: " + device.getDeviceRoom());
-            }
-        } else {
-            System.out.println("GET Request Failed: " + getDiscDevicesStatus.getMessage());
-        }
     }
+
+//        RestWrapper restWrapper = new RestWrapper();
+//
+//        // Test POST request
+//        RequestStatus postStatus = restWrapper.sendPost(RestPath.POST_PATH, "Test Payload");
+//        System.out.println("POST Request Success: " + postStatus.isSuccess());
+//        System.out.println("POST Request Message: " + postStatus.getMessage());
+//
+//        // Test GET request
+//        RequestStatus getStatus = restWrapper.sendGet(RestPath.GET_PATH);
+//        System.out.println("GET Request Success: " + getStatus.isSuccess());
+//        System.out.println("GET Request Message: " + getStatus.getMessage());
+//
+//        // Test GET request for devices
+//        RequestStatus getDiscDevicesStatus = restWrapper.sendGet(RestPath.GET_DISC_DEVICES_PATH);
+//        if (getStatus.isSuccess()) {
+//            Gson gson = new Gson();
+//            Type listType = new TypeToken<List<HouseholdDevice>>() {}.getType();
+//            List<HouseholdDevice> devices = gson.fromJson(getDiscDevicesStatus.getMessage(), listType);
+//
+//            for (HouseholdDevice device : devices) {
+//                System.out.println("Device: " + device.getDeviceName() + ", Room: " + device.getDeviceRoom());
+//            }
+//        } else {
+//            System.out.println("GET Request Failed: " + getDiscDevicesStatus.getMessage());
+//        }
 }
