dfad8485e4a276da64df55944e155821cd2d8aa1
rranl <rranlevi@gmail.com>
2024-08-17 17:10:57 +0300

    Client

--- src/Classes/CameraStream.java
+++ src/Classes/CameraStream.java
@@ -2,10 +2,10 @@ package Classes;
 
 public class CameraStream extends Widget {
     private String currentFrame;
-    private String currentFrameNum;
 
-    public CameraStream(String text) {
+    public CameraStream(String text, String currentFrame) {
         super(text);
+        this.currentFrame = currentFrame;
     }
 
     public String getCurrentFrame() {
@@ -14,13 +14,5 @@ public class CameraStream extends Widget {
 
     public void setCurrentFrame(String currentFrame) {
         this.currentFrame = currentFrame;
-    }
-
-    public String getCurrentFrameNum() {
-        return this.currentFrameNum;
-    }
-
-    public void setCurrentFrameNum(String currentFrameNum) {
-        this.currentFrameNum = currentFrameNum;
     }
 }
